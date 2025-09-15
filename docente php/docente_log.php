8806fa15454a3f42fbee4e9a652ff5774372ca81
alfo0710 <alfonsinacoiro@gmail.com>
2025-09-15 14:04:37 -0300

    Docente con los links actualizados

--- docente php/docente_log.php
+++ docente php/docente_log.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style/style.css">
 </head>
 
 <body>
@@ -16,9 +16,9 @@
   <nav class="d-md-none">
     <div class="container-fluid">
       <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#menuLateral">
-        <img class="menuResponsive" src="img/menu.png" alt="menu">
+        <img class="menuResponsive" src="/img/menu.png" alt="menu">
       </button>
-      <img class="logoResponsive" src="img/logo.png" alt="logoRespnsive">
+      <img class="logoResponsive" src="/img/logo.png" alt="logoRespnsive">
     </div>
   </nav>
 
@@ -28,12 +28,12 @@
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
     </div>
     <div class="offcanvas-body d-flex flex-column">
-      <a href="./index.php" class="mb-3"><i class="bi bi-arrow-left-circle-fill me-2"></i>Volver</a>
+      <a href="../index.php" class="mb-3"><i class="bi bi-arrow-left-circle-fill me-2"></i>Volver</a>
       
-      <a href="./estudiante.php" class="nav-opciones mb-2">Estudiante</a>
-      <a href="./adscripto_log.php" class="nav-opciones mb-2">Adscripto</a>
-      <a href="./docente_log.php" class="fw-semibold seleccionado">Docente</a>
-      <a href="./secretario_log.php" class="nav-opciones mb-2">Secretario</a>
+      <a href="/estudiante php/estudiante.php" class="nav-opciones mb-2">Estudiante</a>
+      <a href="/adscripto php/adscripto_log.php" class="nav-opciones mb-2">Adscripto</a>
+      <a href="/docente php/docente_log.php" class="fw-semibold seleccionado">Docente</a>
+      <a href="/secretario php/secretario_log.php" class="nav-opciones mb-2">Secretario</a>
     </div>
   </div>
 
@@ -45,21 +45,21 @@
       <div class="col-md-3 barra-lateral d-none d-md-flex">
         <div class="volverGeneral">
           <div class="volver">
-            <a href="./index.php"><i class="bi bi-arrow-left-circle-fill icono-volver"></i></a>
-            <a href="./index.php">Volver</a>
+            <a href="../index.php"><i class="bi bi-arrow-left-circle-fill icono-volver"></i></a>
+            <a href="../index.php">Volver</a>
           </div>
         </div>
 
-        <a href="./estudiante.php" class="nav-opciones">Estudiante</a>
-        <a href="./adscripto_log.php" class="nav-opciones">Adscripto</a>
-        <a href="./docente_log.php" class="fw-semibold seleccionado">Docente</a>
-        <a href="./secretario_log.php" class="nav-opciones">Secretario</a>
+        <a href="/estudiante php/estudiante.php" class="nav-opciones">Estudiante</a>
+        <a href="/adscripto php/adscripto_log.php" class="nav-opciones">Adscripto</a>
+        <a href="/docente php/docente_log.php" class="fw-semibold seleccionado">Docente</a>
+        <a href="/secretario php/secretario_log.php" class="nav-opciones">Secretario</a>
       </div>
 
 <!-- Contenido principal -->
 <main class="col-md-9 principal-estudiantes" >
 
-    <img src="img/logo.png" alt="Logo" class="logo"> 
+    <img src="/img/logo.png" alt="Logo" class="logo"> 
     
     <section class="seccion-form docente">
 
