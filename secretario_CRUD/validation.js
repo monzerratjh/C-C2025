0b087fc4196637f5a99f23854492f6a2d4c07320
Monze <monzerratarebalojh@gmail.com>
2025-09-19 13:35:52 -0300

    19.09
    + Validación en javascript para creación y edición de usuarios.

---
+++ secretario_CRUD/validation.js
@@ -0,0 +1,55 @@
+const form = document.querySelector('form');
+const ci_usuario = document.getElementById('ci_usuario');
+const nombre_usuario = document.getElementById('nombre_usuario');
+const apellido_usuario = document.getElementById('apellido_usuario');
+const telefono_usuario = document.getElementById('telefono_usuario');
+const gmail_usuario = document.getElementById('gmail_usuario');
+const contrasenia_usuario = document.getElementById('contrasenia_usuario');
+
+
+function validacionCampos() {
+    if (ci_usuario.value.trim() == "" || 
+        contrasenia_usuario.value.trim() == "" || 
+        nombre_usuario.value.trim() == "" || 
+        apellido_usuario.value.trim() == "" || 
+        telefono_usuario.value.trim() == "" || 
+        gmail_usuario.value.trim() == "") {
+        alert("Por favor, complete todos los campos");
+        return false;
+    }
+    return true;
+}
+
+
+function validacionLongitud(ci_usuario, telefono_usuario) {
+    if (ci_usuario.value.trim().length !== 8 || isNaN(ci_usuario.value.trim())) {
+        alert("La cédula debe tener 8 números válidos");
+        return false;
+    } else if(telefono_usuario.value.trim().length !== 9 || isNaN(telefono_usuario.value.trim())) {
+        alert("El teléfono debe tener 9 números válidos");
+        return false;
+    }
+    return true;
+}
+
+
+function validarConstrasenia (contrasenia_usuario){
+    if (contrasenia_usuario.value.trim().length < 5) {
+        alert("La contraseña debe tener al menos 5 caracteres.");
+            return false; // Evitar el envío del formulario
+        }
+        if (!/[A-Z]/.test(contrasenia_usuario.value.trim()) || !/[0-9]/.test(contrasenia_usuario.value.trim())) {
+        alert("La contraseña debe contener al menos una letra mayúscula y un número.");
+            return false; // Evitar el envío del formulario
+        }
+    return true;
+}
+
+
+form.addEventListener('submit', function(e) {
+    if( !validacionCampos() ||
+        !validacionLongitud(ci_usuario, telefono_usuario) ||
+        !validarConstrasenia(contrasenia_usuario)) {
+        e.preventDefault();
+    }
+});
