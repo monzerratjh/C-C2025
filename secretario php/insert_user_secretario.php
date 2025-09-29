77ba7fea95b704c502a285bf0dd4a874fbce994f
Monze <monzerratarebalojh@gmail.com>
2025-09-29 10:30:26 -0300

    29.09 (6)
    VAMOS LRPM (2)

--- secretario php/insert_user_secretario.php
+++ secretario php/insert_user_secretario.php
@@ -2,7 +2,7 @@
 include('..\conexion.php');
 $conn = conectar_bd();
 
-$id_usuario = null;
+
 $ci_usuario = $_POST['ci_usuario'];
 $nombre_usuario = $_POST['nombre_usuario'];
 $apellido_usuario = $_POST['apellido_usuario'];
@@ -12,31 +12,38 @@ $cargo_usuario = $_POST['cargo_usuario'];
 $contrasenia_usuario = $_POST['contrasenia_usuario'];
 
 $sql = "INSERT INTO usuario 
-      (id_usuario, ci_usuario, nombre_usuario, apellido_usuario, gmail_usuario, telefono_usuario, cargo_usuario, contrasenia_usuario) 
-    VALUES('$id_usuario', '$ci_usuario', '$nombre_usuario', '$apellido_usuario', '$gmail_usuario', '$telefono_usuario','$cargo_usuario','$contrasenia_usuario')";
+      (ci_usuario, nombre_usuario, apellido_usuario, gmail_usuario, telefono_usuario, cargo_usuario, contrasenia_usuario) 
+    VALUES('$ci_usuario', '$nombre_usuario', '$apellido_usuario', '$gmail_usuario', '$telefono_usuario','$cargo_usuario','$contrasenia_usuario')";
 $query = mysqli_query($conn, $sql);
+$idUsuario = $conn->insert_id;
 
 if ($cargo_usuario === "Docente") {
     $grado = $_POST['grado_docente_usuario'] ?? null;
-    $sql_docente = "INSERT INTO docente (id_usuario, grado_docente_usuario) 
-                    VALUES ('$id_usuario', '$grado')";
-    mysqli_query($conn, $sql_docente);
+    $sql_docente = "INSERT INTO docente (grado_docente, id_usuario) 
+                    VALUES ($grado, $idUsuario);";
+    $queryResult = mysqli_query($conn, $sql_docente);
 
 } elseif ($cargo_usuario === "Adscripto") {
     $cantidad_grupo_adscripto = $_POST['cantidad_grupo_adscripto'] ?? null;
-    $horario_adscripto = $_POST['horario_adscripto_usuario'] ?? null;
+    $horario_entrada_adscripto = $_POST['horario_entrada_adscripto'] ?? null;
+    $horario_entrada_adscripto .= ":00";
+    $horario_salida_adscripto = $_POST['horario_salida_adscripto'] ?? null;
+    $horario_salida_adscripto .= ":00";
     $caracter_cargo_adscripto = $_POST['caracter_cargo_adscripto'] ?? null;
 
-    $sql_adscripto = "INSERT INTO adscripto (id_usuario, cantidad_grupo_adscripto, horario_adscripto_usuario, caracter_cargo_adscripto) 
-                      VALUES ('$id_usuario',$cantidad_grupo_adscripto', '$horario_adscripto', '$caracter_cargo_adscripto')";
+    $sql_adscripto = "INSERT INTO adscripto (cantidad_grupos_asignados, horario_entrada_adscripto, horario_salida_adscripto, caracter_cargo_adscripto, id_usuario) 
+                      VALUES ($cantidad_grupo_adscripto, '$horario_entrada_adscripto', '$horario_salida_adscripto', '$caracter_cargo_adscripto', $idUsuario)";
     mysqli_query($conn, $sql_adscripto);
 
 } elseif ($cargo_usuario === "Secretario") {
-    $horario_secretario = $_POST['horario_secretario_usuario'] ?? null;
-    $cargo_secretario = $_POST['cargo_secretario_usuario'] ?? null;
-
-    $sql_secretario = "INSERT INTO secretario (id_usuario, horario_secretario_usuario, cargo_secretario_usuario) 
-                       VALUES (''$id_usuario',$horario_secretario', '$cargo_secretario')";
+    $horario_entrada_secretario = $_POST['horario_entrada_secretario'] ?? null;
+    $horario_entrada_secretario .= ":00";
+    $horario_salida_secretario = $_POST['horario_salida_secretario'] ?? null;
+    $horario_salida_secretario .= ":00";
+    $grado_secretario = $_POST['grado_secretario'] ?? null;
+    
+    $sql_secretario = "INSERT INTO secretario (horario_entrada_secretario, horario_salida_secretario, grado_secretario, id_usuario) 
+                       VALUES ('$horario_entrada_secretario','$horario_salida_secretario', '$grado_secretario', $idUsuario)";
     mysqli_query($conn, $sql_secretario);
 }
 
