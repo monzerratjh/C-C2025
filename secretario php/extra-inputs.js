a72cae26a19668605ebe360e66c990b15a99d247
Monze <monzerratarebalojh@gmail.com>
2025-09-29 10:30:05 -0300

    29.09 (5)
    VAMOOOOOS LRPM

--- secretario php/extra-inputs.js
+++ secretario php/extra-inputs.js
@@ -10,18 +10,18 @@ select_cargo_usuario = document.getElementById('usuarioTipo');
                 extra_inputs.innerHTML = `
                 <input type="number" name="cantidad_grupo_adscripto" placeholder="Cantidad de grupos a cargo" id="cantidad_grupo_adscripto" >
                 <label>Horario de Entrada:</label>
-                <input type="time" name="horario_adscripto_usuario" id="horario_entrada_adscripto_usuario" >
+                <input type="time" name="horario_entrada_adscripto" id="horario_entrada_adscripto">
                 <label>Horario de Salida:</label>
-                <input type="time" name="horario_adscripto_usuario" id="horario_salida_adscripto_usuario" >
+                <input type="time" name="horario_salida_adscripto" id="horario_salida_adscripto" >
                 <input type="text" name="caracter_cargo_adscripto" placeholder="Cargo" id="caracter_cargo_adscripto" >
                 `;
             } else if(select_cargo_usuario.value == 'Secretario') {
                 extra_inputs.innerHTML = `
                 <label>Horario de Entrada:</label>
-                <input type="time" name="horario_adscripto_usuario" id="horario_entrada_adscripto_usuario" >
+                <input type="time" name="horario_entrada_secretario" id="horario_entrada_secretario" >
                 <label>Horario de Salida:</label>
-                <input type="time" name="horario_adscripto_usuario" id="horario_salida_adscripto_usuario" >
-                <input type="number" name="cargo_secretario_usuario" placeholder="Cargo de Secretario" id="cargo_secretario_usuario" >
+                <input type="time" name="horario_salida_secretario" id="horario_salida_secretario" >
+                <input type="number" name="grado_secretario" placeholder="Grado de Secretario" id="grado_secretario" >
                 `;
             } else if(select_cargo_usuario.value == '') {
                 //no sé
