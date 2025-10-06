6bf7ebbd246e509880cbfc644b723d421e4b1e55
alfo0710 <alfonsinacoiro@gmail.com>
2025-10-06 17:32:22 -0300

    prueba index y estudiante

--- users/estudiante/horario/horario3MB.php
+++ users/estudiante/horario/horario3MB.php
@@ -13,7 +13,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     <!-- CSS propio -->
-    <link rel="stylesheet" href="/general/css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
