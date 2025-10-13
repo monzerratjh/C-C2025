b880e090cfdc3e954803cfd56408f7b275379124
alfo0710 <alfonsinacoiro@gmail.com>
2025-10-13 09:51:58 -0300

    secretario bienvenida prueba

--- users/secretario/secretario-bienvenida.php
+++ users/secretario/secretario-bienvenida.php
@@ -7,7 +7,7 @@ $nombre_secretario = $_SESSION['nombre_usuario'] ?? 'Secretario/a';
     <h1>Bienvenido, <?php echo htmlspecialchars($nombre_secretario); ?></h1>
 
 */
-
+include('./../../conexion.php');
 //include('../../encabezado.php');
 ?>
 <!DOCTYPE html>
@@ -21,7 +21,7 @@ $nombre_secretario = $_SESSION['nombre_usuario'] ?? 'Secretario/a';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     <!-- CSS propio -->
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="./../../css/style.css">
 </head>
 
 <body>
