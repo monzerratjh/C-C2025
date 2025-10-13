6ede28ffffbe18f75ce4a80d0d91f9c1010a2c2e
alfo0710 <alfonsinacoiro@gmail.com>
2025-10-13 10:03:13 -0300

    modserver3

--- users/secretario/secretario-bienvenida.php
+++ users/secretario/secretario-bienvenida.php
@@ -11,7 +11,7 @@ $nombre_secretario = $_SESSION['nombre_usuario'] ?? 'Secretario/a';
 //include('../../encabezado.php');
 
 */
-include('./../../conexion.php');
+include('./../../backend-resources/conexion.php');
 ?>
 <!DOCTYPE html>
 <html lang="es">
