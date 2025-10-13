14326b0316ec463b3f5a4de6bed4e4c2d75396f8
alfo0710 <alfonsinacoiro@gmail.com>
2025-10-13 09:08:16 -0300

    probando encabezado

--- users/adscripto/adscripto-log.php
+++ users/adscripto/adscripto-log.php
@@ -1,5 +1,5 @@
 <?php 
-//include('./encabezado.php');
+include('./../../encabezado.php');
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -7,12 +7,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel Adscriptos</title>
-    <!-- Bootstrap CSS + Iconos + letras-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-    <!-- CSS propio -->
-    <link rel="stylesheet" href="./../../css/style.css">
 </head>
 
 <body>
