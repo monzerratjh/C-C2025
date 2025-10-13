26484490f8007a4dbd55ee3e8d3ab5d17c56b887
alfo0710 <alfonsinacoiro@gmail.com>
2025-10-13 08:45:23 -0300

    actualizacion estudiantes

--- users/estudiante/horario/horario3MD.php
+++ users/estudiante/horario/horario3MD.php
@@ -21,9 +21,9 @@
   <nav class="d-md-none">
     <div class="container-fluid">
       <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#menuLateral">
-        <img class="menuResponsive" src="./../../../img/menu.png" alt="menu">
+        <img class="menuResponsive" src="./../../img/menu.png" alt="menu">
       </button>
-      <img class="logoResponsive" src="./../../../img/logo.png" alt="logoRespnsive">
+      <img class="logoResponsive" src="./../../img/logo.png" alt="logoRespnsive">
     </div>
   </nav>
 
