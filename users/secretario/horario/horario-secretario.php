da80e17355de8635ad15bfebf3a83e4fcabd9ea1
alfo0710 <alfonsinacoiro@gmail.com>
2025-10-14 09:50:43 -0300

    finalizada pagian horarios

--- users/secretario/horario/horario-secretario.php
+++ users/secretario/horario/horario-secretario.php
@@ -96,9 +96,6 @@ $con->close();
             <th>Hora de inicio (por hora)</th>
             <th>Hora de finalización (por hora)</th>
             <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
             <th></th> 
           </tr>
           
@@ -108,8 +105,6 @@ $con->close();
           <tr>
             <td><?= htmlspecialchars($filaHorario['hora_inicio']) ?></td>
             <td><?= htmlspecialchars($filaHorario['hora_fin']) ?></td>
-            <td></td>
-            <td></td>
             <td>
               <button class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#modalHorario"
                 onclick="cargarEditarHorario(
@@ -132,15 +127,16 @@ $con->close();
           </tr>
           <?php endwhile; ?>
 
-          <tr>
-            <th></th>
-        <th><h2 class="agregar" data-bs-toggle="modal" data-bs-target="#modalHorario" onclick="document.getElementById('accionHorario').value='insertar';">+</h2></th>
-            <th></th>
-            <th></th>
-             <th></th>
-            <th></th>
-            <th></th>
-          </tr>
+        <tr>
+ <td colspan="7" class="text-center"> <!-- Une todas las columnas en una sola celda y centra el "+" -->
+     <h4 class="agregar"
+        data-bs-toggle="modal"
+        data-bs-target="#modalHorario"
+        onclick="document.getElementById('accionHorario').value='insertar';">
+      +
+      </h4>
+  </td>
+</tr>
         </tbody>
       </table>
     </main>
