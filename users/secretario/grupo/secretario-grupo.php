3465266123861871161d0b18c6a10c72bdad5090
TatianadeFreitas <maitetorrespipo@gmail.com>
2025-10-16 14:24:15 -0300

    Responsive prueba

--- users/secretario/grupo/secretario-grupo.php
+++ users/secretario/grupo/secretario-grupo.php
@@ -97,14 +97,16 @@ $con->close(); // cierro conexión cuando ya tengo todos los datos
 
 
 <!DOCTYPE html>
+<html lang="es"></html>
 <head>
+   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title> Grupos </title>
-    <!-- Bootstrap CSS + Iconos + letras-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-    <!-- CSS propio -->
-    <link rel="stylesheet" href="./../../../css/style.css">
+  <!-- Bootstrap CSS + Iconos + letras -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./../../../css/style.css">
 </head>
 <body>
 
