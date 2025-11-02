129e5cc3ad0ce5c58ce521873251ed29092a5e44
Sabrina Maristan <sabrinamaristan1696@gmail.com>
2025-11-02 16:53:31 -0300

    prueba4

--- utils/log-out.php
+++ utils/log-out.php
@@ -2,6 +2,9 @@
 session_start();
 session_unset();
 session_destroy();
-header('Location: ./../index.php');
+
+// Detectar automáticamente la raíz del proyecto
+$rutaIndex = str_repeat('../', substr_count($_SERVER['PHP_SELF'], '/')) . 'index.php';
+header("Location: $rutaIndex");
 exit();
 ?>
