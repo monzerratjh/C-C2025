9a312b37c6c13bcb3c2c088ccfe1fca6b4ad42d2
Sabrina Maristan <sabrinamaristan1696@gmail.com>
2025-11-02 16:57:36 -0300

    prueba5

--- utils/log-out.php
+++ utils/log-out.php
@@ -3,8 +3,13 @@ session_start();
 session_unset();
 session_destroy();
 
-// Detectar automáticamente la raíz del proyecto
-$rutaIndex = str_repeat('../', substr_count($_SERVER['PHP_SELF'], '/')) . 'index.php';
-header("Location: $rutaIndex");
+// Construir ruta completa a la raíz del proyecto
+$host = $_SERVER['HTTP_HOST']; // dbitsp.tailff9876.ts.net
+$path = dirname($_SERVER['PHP_SELF']); // /CoffeeAndCode/C-C2025/users/docente/reserva
+// Subir hasta la raíz del proyecto
+$basePath = '/CoffeeAndCode/C-C2025/';
+
+// Redirigir correctamente
+header("Location: http://$host$basePath" . "index.php");
 exit();
 ?>
