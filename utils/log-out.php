6f8d97488800080126c2009845dda96a3b69df75
TatianadeFreitas <maitetorrespipo@gmail.com>
2025-11-03 09:57:51 -0300

    banner en editar-propiedad-espacio

--- utils/log-out.php
+++ utils/log-out.php
@@ -4,8 +4,8 @@ session_unset();
 session_destroy();
 
 // Construir ruta completa a la raíz del proyecto
-$host = $_SERVER['HTTP_HOST']; // dbitsp.tailff9876.ts.net
-$path = dirname($_SERVER['PHP_SELF']); // /CoffeeAndCode/C-C2025/users/docente/reserva
+$host = $_SERVER['HTTP_HOST']; // dbitsp.tailff9876.ts.net (Detectar el dominio / servidor)
+$path = dirname($_SERVER['PHP_SELF']); // /CoffeeAndCode/C-C2025/users/docente/reserva (Detectar la carpeta donde está el archivo actual)
 // Subir hasta la raíz del proyecto
 $basePath = '/CoffeeAndCode/C-C2025/';
 
